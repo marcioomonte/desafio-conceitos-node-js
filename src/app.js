545839fb4e61fe04d3fb6d919c84df3241e1e0ef
Marcio Monte <marciomonte@proabakus.com.br>
2021-03-07 01:27:24 -0300

    concluindo desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,76 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  // const { id } = request.params;
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs: [...techs],
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const matchRepoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (matchRepoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found' });
+  }
+
+  if (Object.keys(request.body).includes('likes')) {
+    response.status(400).json({ likes: repositories[matchRepoIndex].likes });
+  }
+
+  const { title, url, techs } = request.body;
+
+  const repository = { id, title, url, techs };
+
+  repositories[matchRepoIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const matchRepoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (matchRepoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(matchRepoIndex, 1);
+
+  response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const matchRepoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (matchRepoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found' });
+  }
+  repositories[matchRepoIndex].likes = repositories[matchRepoIndex].likes + 1;
+
+  response.status(201).json({ likes: repositories[matchRepoIndex].likes });
 });
 
 module.exports = app;
